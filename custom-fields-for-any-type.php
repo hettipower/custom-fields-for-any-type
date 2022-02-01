78c429cf32cfc0a3d92b39b85eb20a27a04f284c
Tharindu Hettiarachchi <hettipower@gamil.com>
2022-02-01 22:51:39 +0530

    new fields group added

--- custom-fields-for-any-type.php
+++ custom-fields-for-any-type.php
@@ -11,10 +11,12 @@ Text Domain: cffapt
 
 defined( 'ABSPATH' ) or exit;
 define( 'CFFAPT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'CFFAPT_URL', plugin_dir_url( __FILE__ ) );
 
 //Settings Options
 require_once CFFAPT_PATH.'/admin/options/admin_options.php';
 //Custom Meta Fields
-require_once CFFAPT_PATH.'/admin/meta-fields/custom_meta_fields.php';
+require_once CFFAPT_PATH.'/admin/meta-fields/custom_meta_links_list.php';
+//require_once CFFAPT_PATH.'/admin/meta-fields/custom_meta_fields.php';
 //Admin Scripts
 require_once CFFAPT_PATH.'/admin/scripts/admin_scripts.php';
