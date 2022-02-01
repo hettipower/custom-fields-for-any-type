132ac96d79415641b02957d806fbbf1af1c0277d
Tharindu Hettiarachchi <hettipower@gamil.com>
2022-02-01 22:49:49 +0530

    repeater config js

---
+++ admin/assets/js/cffapt-admin-post.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+    $('.repeater').repeater({
+        // (Optional)
+        // start with an empty list of repeaters. Set your first (and only)
+        // "data-repeater-item" with style="display:none;" and pass the
+        // following configuration flag
+        initEmpty: false,
+        show: function () {
+            $(this).slideDown();
+        },
+        hide: function (deleteElement) {
+            if(confirm('Are you sure you want to delete this element?')) {
+                $(this).slideUp(deleteElement);
+            }
+        },
+        isFirstItemUndeletable: true
+    })
+});
